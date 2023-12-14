eb41d6425d47e27dcbbf9ec436e668cb5fd78280
lake041 <lake041@korea.ac.kr>
2023-12-14 16:53:25 +0900

    Fix: 관리자 페이지

--- sesac-docent-backend/src/main/java/com/fred/docent/controller/PostController.java
+++ sesac-docent-backend/src/main/java/com/fred/docent/controller/PostController.java
@@ -74,6 +74,7 @@ public class PostController {
 			@PathVariable("category") Integer category) {
 		FetchPostDetailsRequestDTO requestDTO = new FetchPostDetailsRequestDTO(postId, category);
 		List<FetchPostDetailsResponseDTO> postDetails = postsService.fetchPostDetails(requestDTO);
+		log.info(requestDTO);
 		if (postDetails != null) {
 			return new ResponseEntity<>(postDetails, HttpStatus.OK);
 		} else {
@@ -83,20 +84,13 @@ public class PostController {
 
 	@GetMapping(value = "/listup/{p_table_name}/{p_page_size}/{p_page_number}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<FetchArtCollectionResponseDTO>> fetchArtCollections(
-	        @PathVariable("p_table_name") String tablename, 
-	        @PathVariable("p_page_size") int pageSize,
-	        @PathVariable("p_page_number") int pageNumber,
-	        @RequestParam(required = false) String searchTitle) {
-	    
-	    FetchPostsRequestDTO requestDTO = FetchPostsRequestDTO.builder()
-	            .p_table_name(tablename)
-	            .p_page_size(pageSize)
-	            .p_page_number(pageNumber)
-	            .p_search_title(searchTitle)
-	            .build();
-	    List<FetchArtCollectionResponseDTO> postsResponse = postsService.fetchArtCollections(requestDTO);
-	    return new ResponseEntity<>(postsResponse, HttpStatus.OK);
-	}
+			@PathVariable("p_table_name") String tablename, @PathVariable("p_page_size") int pageSize,
+			@PathVariable("p_page_number") int pageNumber, @RequestParam(required = false) String searchTitle) {
 
+		FetchPostsRequestDTO requestDTO = FetchPostsRequestDTO.builder().p_table_name(tablename).p_page_size(pageSize)
+				.p_page_number(pageNumber).p_search_title(searchTitle).build();
+		List<FetchArtCollectionResponseDTO> postsResponse = postsService.fetchArtCollections(requestDTO);
+		return new ResponseEntity<>(postsResponse, HttpStatus.OK);
+	}
 
 }
